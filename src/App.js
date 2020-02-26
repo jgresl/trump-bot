c1e1eb0505f92a9ebf0d1fa2af3b5b1b8b40fa79
SlimShamy <43048215+SlimShamy@users.noreply.github.com>
2020-02-26 11:37:02 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -58,24 +58,48 @@ function PrintTreeRecursive(node, level) {
   });
 }
 
-  function mapArrayToTree(arr, root) {
-    let parent = root;
-    //for each string in array
-    arr.forEach(txt => {
-      //check if the string exists in the tree
-      let node = parent.children.find(child => {
-        return child.data === txt;
-      });
-      if (node) {
-        parent = node;
-      }
+  function Map(arr, root) {
+    let outputset = [];
+    root.children.forEach(child => {
+      outputset.push(recursiveMap(arr, child));
     });
+    return outputset;
+  }
+
+  // recursive helper method for Map
+  function recursiveMap(arr, node) {
+    let outputset = [];
+    //if node is a leaf return node.data to output set
+    if (isLeaf(node)) {
+      outputset.push(node.data);
+    //if node is not a leaf proceed to mapping
+    } else {
+      //find if node.data matches a value in the array
+      let match = arr.find(txt => {
+        return txt === node.data;
+      });
+      //if a match is found recursively map from each child of node
+      if (match) {
+        //for each child of node
+        node.children.forEach(child => {
+          recursiveMap(arr, child);
+        });
+      }
+    }
+    return outputset;
+  }
+
+  function isLeaf(node) {
+    if (!node.children.length) {
+      return true;
+    }
   }
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.debugResponseTree = this.debugResponseTree.bind(this);
+    this.debugMap = this.debugMap.bind(this);
   }
 
   componentDidMount() {
@@ -91,11 +115,17 @@ class App extends React.Component {
     PrintTree(this.tree);
   }
 
+  debugMap() {
+    let arr = ['category1', 'topic1', 'category2', 'topic2'];
+    console.log(Map(arr, this.tree));
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <button onClick={this.debugResponseTree}>Print the tree!</button>
+          <button onClick={this.debugMap}>Should return outputs 1,2,4</button>
         </header>
       </div>
     );
