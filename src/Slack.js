260367d751ec7419ec2a9fce9011603fb4fe3d2d
SlimShamy <43048215+SlimShamy@users.noreply.github.com>
2020-03-18 12:39:58 -0700

    integrated with bot code

--- src/Slack.js
+++ src/Slack.js
@@ -1,18 +1,27 @@
 const { App } = require('@slack/bolt');
+const fetch = require('node-fetch');
+var lastResponse = '';
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
-  token: 'xoxb-919861223334-993623735377-KB9SgCy5j99ZHgbCwRI9d1fB',
-  signingSecret: 'c865622026ac42e48f1ad2b7d8e53227'
+  token: 'xoxb-755200714180-1009768874725-MNY6gFJwmqgtrTvun59Laob1',
+  signingSecret: '50ef9ff2b498e3cb6ebc96491f64eed8'
 });
 
 (async () => {
   // Start your app
-  await app.start(process.env.PORT || 3000);
+  await app.start(process.env.PORT || 16000);
 
   console.log('⚡️ Bolt app is running!');
 })();
 
-app.message('@trump', ({message, say}) => {
-  say(`Hey there <@${message.user}>!`);
+app.event('app_mention', async ({event, say}) => {
+  fetch(`http://localhost:17000?input=${event.text}&lastResponse=${lastResponse}`)
+    .then(response => {
+      return response.text();
+    })
+    .then(output => {
+      say(output);
+      lastResponse = output;
+      });
 });
