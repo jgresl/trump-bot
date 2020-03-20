8716ce9e2dc74dccdd9f2edc03d5a443fa1ef748
SlimShamy <43048215+SlimShamy@users.noreply.github.com>
2020-03-20 14:51:35 -0700

    added .env file

--- src/Slack.js
+++ src/Slack.js
@@ -1,11 +1,12 @@
 const { App } = require('@slack/bolt');
 const fetch = require('node-fetch');
+require('dotenv').config()
 var lastResponse = '';
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
-  token: 'xoxb-755200714180-1009768874725-toSW2OiQ5jJl6vKeuULLsLPL',
-  signingSecret: '50ef9ff2b498e3cb6ebc96491f64eed8'
+  token: process.env.token,
+  signingSecret: process.env.signingSecret
 });
 
 (async () => {
@@ -15,7 +16,7 @@ const app = new App({
   console.log('⚡️ Bolt app is running!');
 })();
 
-//Wait to be mentioned in chat
+//Wait to be mentioned in chat 
 app.event('app_mention', async ({event, say}) => {
   //Get response from server
   fetch(`http://localhost:17000?input=${event.text}&lastResponse=${lastResponse}`)
